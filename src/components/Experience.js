0d8ea0997a3bf60e88a5ca23a659ed89357084bd
MICHAEL ROARK <Roarkmichael4@gmail.com>
2024-03-29 12:35:25 -0700

    trying to fix the error

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,52 +8,43 @@ import Badge from "react-bootstrap/Badge";
 
 class Experience extends Component {
   render() {
-    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.experience;
-      var work = this.props.resumeExperience.map(function (work, i) {
-        const technologies = work.technologies;
-        const mainTechnologies = work.mainTech;
+    let workElements = null;
+    let sectionName = "";
 
-        var mainTech = mainTechnologies.map((technology, i) => {
-          return (
-            <Badge pill className="main-badge mr-2 mb-2" key={i}>
-              {technology}
-            </Badge>
-          );
-        });
-        var tech = technologies.map((technology, i) => {
-          return (
-            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
-              {technology}
-            </Badge>
-          );
-        });
+    if (this.props.resumeExperience && this.props.resumeBasicInfo && this.props.resumeBasicInfo.section_name) {
+      sectionName = this.props.resumeBasicInfo.section_name.experience;
+      
+      workElements = this.props.resumeExperience.map(function (work, i) {
+        const technologies = work.technologies || [];
+        const mainTechnologies = work.mainTech || [];
+
+        var mainTech = mainTechnologies.map((technology, i) => (
+          <Badge pill className="main-badge mr-2 mb-2" key={i}>
+            {technology}
+          </Badge>
+        ));
+
+        var tech = technologies.map((technology, i) => (
+          <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+            {technology}
+          </Badge>
+        ));
+
         return (
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date={work.years}
-            iconStyle={{
-              background: "#AE944F",
-              color: "#fff",
-              textAlign: "center",
-            }}
+            iconStyle={{ background: "#AE944F", color: "#fff", textAlign: "center" }}
             icon={<i className="fab fa-angular experience-icon"></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
               {mainTech}
             </div>
-
-            <h3
-              className="vertical-timeline-element-title"
-              style={{ textAlign: "left" }}
-            >
+            <h3 className="vertical-timeline-element-title" style={{ textAlign: "left" }}>
               {work.title}
             </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              style={{ textAlign: "left" }}
-            >
+            <h4 className="vertical-timeline-element-subtitle" style={{ textAlign: "left" }}>
               {work.company}
             </h4>
             <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
@@ -66,25 +57,17 @@ class Experience extends Component {
       <section id="resume" className="pb-5">
         <div className="col-md-12 mx-auto">
           <div className="col-md-12">
-            <h1 className="section-title" style={{ color: "black" }}>
-              <span className="text-black" style={{ textAlign: "center" }}>
-                {sectionName}
-              </span>
+            <h1 className="section-title" style={{ color: "black", textAlign: "center" }}>
+              {sectionName}
             </h1>
           </div>
         </div>
         <div className="col-md-8 mx-auto">
           <VerticalTimeline>
-            {work}
+            {workElements}
             <VerticalTimelineElement
-              iconStyle={{
-                background: "#AE944F",
-                color: "#fff",
-                textAlign: "center",
-              }}
-              icon={
-                <i className="fas fa-hourglass-start mx-auto experience-icon"></i>
-              }
+              iconStyle={{ background: "#AE944F", color: "#fff", textAlign: "center" }}
+              icon={<i className="fas fa-hourglass-start mx-auto experience-icon"></i>}
             />
           </VerticalTimeline>
         </div>
